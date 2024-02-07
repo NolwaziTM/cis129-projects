b0a9fefa864c6a9a60a0573e7720013cd733173f
NolwaziTM <158247691+NolwaziTM@users.noreply.github.com>
2024-02-07 13:32:03 -0700

    Update cis129_lab02_shortStory.py
    Within my repository, I navigate to the Code tab and select the dropdown menu labeled "main." I then enter a descriptive branch name like "code-edits" and create the branch. In the new branch, I access the cis129_lab02_shortStory.py file, remove the print("Hello, World!") statement, and craft a brief narrative using print() statements. Before the code, I add comments documenting my name, program description, and summary of changes. Finally, I commit the changes with a clear message detailing the modifications made.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello,World!')
+#Author:Nolwazi Moyo
+print('my name is Nolwazi')
+#My First Python lab in my CIS129 course
+print('This is my first python lab in my CIS129 course')
+#Prints a simple short story
+print('With the help of this course I plan to expand my understanding of codes written in computers') 
